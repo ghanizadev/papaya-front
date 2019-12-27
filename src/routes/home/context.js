5d529dadfffeb1c7b5dbaded107ec8973eb26f8f
ghanizadev <jeanmelo@ghanizadev.com.br>
2019-12-27 01:57:47 -0300

    router + auth flow

---
+++ src/routes/home/context.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+
+const config = {
+	overlay:{
+		visible: false,
+		message: "MENSAGEM",
+		callback: ()=> {}
+    },
+    setOverlay: () => {}
+}
+
+const Context = React.createContext(config);
+
+const Provider = props =>{
+    const setOverlay = (newConfig) => {
+        setState({...state, overlay: newConfig})
+    }
+    
+    const [state, setState] = useState({...config, setOverlay});
+
+    return (
+    <Context.Provider value={state}>
+        {props.children}
+    </Context.Provider>)
+}
+
+const Consumer = props => 
+    <Context.Consumer>
+        {props.children}
+    </Context.Consumer>
+
+export {Context, Provider, Consumer}
